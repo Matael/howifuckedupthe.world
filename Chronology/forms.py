8bb35f9c8d28af75e60e5f7bfa8c714ef395bbe9
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2017-02-13 22:03:10 +0100

    Refactor add popup and form

--- Chronology/forms.py
+++ Chronology/forms.py
@@ -5,11 +5,14 @@ from .models import Event
 
 class EventForm(forms.Form):
 
-    date = forms.DateField(widget=forms.DateInput(attrs={'id': 'datepicker'}))
-    name = forms.CharField(max_length=200)
-    who = forms.CharField(max_length=200, widget=forms.DateInput(attrs={'id': 'autocomp_who'}))
-    description = forms.CharField(widget=forms.Textarea)
-    function = forms.CharField(max_length=200, widget=forms.DateInput(attrs={'id': 'autocomp_function'}))
+    name = forms.CharField(label="Summary", max_length=200,
+                           widget=forms.TextInput(attrs={'placeholder': 'Short summary'}))
+    date = forms.DateField(widget=forms.DateInput(attrs={'placeholder': 'Date',  'id': 'datepicker'}))
+    who = forms.CharField(max_length=200,
+                          widget=forms.DateInput(attrs={'placeholder': 'Who did that?', 'id': 'autocomp_who'}))
+    function = forms.CharField(max_length=200,
+                               widget=forms.DateInput(attrs={'placeholder': 'Position at the time', 'id': 'autocomp_function'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Tell us more about it...'}))
 
 
 
